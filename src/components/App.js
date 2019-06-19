dbef12f1b4b8bab0d2373c92d685b8a6c592dc41
udeepbahadur7 <udeepbahadur7@gmail.com>
2019-06-19 23:01:56 +0545

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import { Header, Footer } from "./layouts";
+
+export default class extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+
+        <Footer />
+      </Fragment>
+    );
+  }
+}
