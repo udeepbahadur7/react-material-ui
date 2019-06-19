c7149f16d69836a15970b25beeb715ef6328cf34
udeepbahadur7 <udeepbahadur7@gmail.com>
2019-06-19 23:04:55 +0545

    initial setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from "react";
 import { Header, Footer } from "./layouts";
-
+import Exercise from "./Exercise";
 export default class extends Component {
   render() {
     return (
       <Fragment>
         <Header />
-
+        <Exercise />
         <Footer />
       </Fragment>
     );
